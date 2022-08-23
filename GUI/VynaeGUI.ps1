a02d2c02cd37bb964a7d2d464b676b47be8b8ffe
Liam Powell <Powell.liam.p@gmail.com>
2022-08-23 00:55:25 -0500

    Create VynaeGUI.ps1
    idk what I'm doing

---
+++ GUI/VynaeGUI.ps1
@@ -0,0 +1,219 @@
+# Memory dumps with procdump 
+# Pass process ID to procdump -- NOT THE NAME
+
+# Description & Goals 
+# List processes and allow user to select a process from the list and dump the memory using procdump
+
+
+# GUI Init
+function GuiInit{
+
+    # Global Form creation
+    Add-Type -assembly System.Windows.Forms
+    $global:mainform = New-Object System.Windows.Forms.Form
+
+    # Form Settings
+    $mainform.Text ="Vynae Process Exploration GUI"
+    $mainform.Width = 1200
+    $mainform.Height = 900
+    $mainform.MaximizeBox = $false
+    $mainform.MinimizeBox = $false
+    $mainform.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
+}
+
+# Gui Start
+function StartForm{
+
+    # Display Form
+    $mainform.ShowDialog()
+
+}
+
+
+# Application MainBody 
+function Proc_Get{
+
+    # Create a label for process info
+    $ProcLabel = New-Object System.Windows.Forms.Label
+    $ProcLabel.Text = "Process Information"
+
+    # Place Label
+    $ProcLabel.Location = New-Object System.Drawing.Point(5,0)
+
+    # Label Settings
+    $ProcLabel.AutoSize = $true
+
+    # Add to main form
+    $mainform.Controls.Add($ProcLabel)
+
+    # Create a list of processes 
+    $global:ProcList = New-Object System.Windows.Forms.datagridview
+
+    # Process list Settings
+    $ProcList.Width = $mainform.Width * .96
+    $ProcList.Height = $mainform.Height * .40
+    $ProcList.ReadOnly = $true
+    # $ProcList.SelectionMode = [System.Windows.Forms.DataGridViewSelectionMode]::FullRowSelect
+
+    # Create Data Array of Process Information
+    $ProcData = Get-Ciminstance CIM_Process | Select Name, ProcessID, ParentProcessID, ExecutablePath, CreationDate
+    $ProcDataArray = New-Object System.Collections.ArrayList
+    $ProcDataArray.AddRange($ProcData)
+
+    # Add array as data source for Data Grid View Object
+    $ProcList.DataSource = $ProcDataArray
+
+    # Resize the Data Grid columns to fill the object space
+    $ProcList.AutoSizeColumnsMode = [System.Windows.Forms.DataGridViewAutoSizeColumnsMode]::Fill
+
+    # Place List 
+    $ProcList.Location = New-Object System.Drawing.Point(10,20)
+
+    # Add to Form
+    $mainform.Controls.Add($ProcList)
+
+
+    # Create Buttons
+    $DumpButton = New-Object System.Windows.Forms.Button
+
+    # Highlighted Process Information
+
+    # Create title label
+    $DetailsLabel = New-Object System.Windows.Forms.button
+    $DetailsLabel.Text = "Process Details"
+    $DetailsLabel.AutoSize = $true
+
+    # Create Info Labels
+    $NameLabel = New-Object System.Windows.Forms.Label
+    $IdLabel = New-Object System.Windows.Forms.Label
+    $ParentID = New-Object System.Windows.Forms.Label
+    $ParentName = New-Object System.Windows.Forms.Label
+    $CreationDate = New-Object System.Windows.Forms.Label
+    $Hash = New-Object System.Windows.Forms.Label
+    $Location = New-Object System.Windows.Forms.Label
+    $CommandLine = New-Object System.Windows.Forms.Label
+
+    # Create Value lables
+    $global:NameLabelValue = New-Object System.Windows.Forms.Label
+    $global:IDLabelValue = New-Object System.Windows.Forms.Label
+    $global:ParentIDValue = New-Object System.Windows.Forms.Label
+    $global:ParentNameValue = New-Object System.Windows.Forms.Label
+    $global:CreationDateValue = New-Object System.Windows.Forms.Label
+    $global:HashValue = New-Object System.Windows.Forms.Label
+    $global:LocationValue = New-Object System.Windows.Forms.Label
+    $global:CommandLineValue = New-Object System.Windows.Forms.Label
+
+    # Init Labels
+    $NameLabel.Text = "Name:"
+    $NameLabel.AutoSize = $true
+    $NameLabel.Location = New-Object System.Drawing.Point(10, 450)
+    $mainform.Controls.Add($NameLabel)
+
+    $NameLabelValue.Text = ""
+    $NameLabelValue.AutoSize = $true
+    $NameLabelValue.Location = New-Object System.Drawing.Point(50, 450)
+    $mainform.Controls.Add($NameLabelValue)
+
+    $IDLabel.Text = "Process ID:"
+    $IDLabel.AutoSize = $true
+    $IDLabel.Location = New-Object System.Drawing.Point(10, 470)
+    $mainform.Controls.Add($IDLabel)
+
+    $IDLabelValue.Text = ""
+    $IDLabelValue.AutoSize = $true
+    $IDLabelValue.Location = New-Object System.Drawing.Point(75, 470)
+    $mainform.Controls.Add($IDLabelValue)
+
+    $ParentID.Text = "Parent ID:"
+    $ParentID.AutoSize = $true
+    $PArentID.Location = New-Object System.Drawing.Point(200, 470)
+    $mainform.Controls.Add($ParentID)
+
+    $ParentIDValue.Text = ""
+    $ParentIDValue.AutoSize = $true
+    $PArentIDValue.Location = New-Object System.Drawing.Point(255, 470)
+    $mainform.Controls.Add($ParentIDValue)
+
+    $ParentName.Text = "Parent Name:"
+    $ParentName.AutoSize = $true
+    $ParentName.Location = New-Object System.Drawing.Point(200, 450)
+    $mainform.Controls.Add($ParentName)
+
+    $ParentNameValue.Text = ""
+    $ParentNameValue.AutoSize = $true
+    $ParentNameValue.Location = New-Object System.Drawing.Point(280, 450)
+    $mainform.Controls.Add($ParentNameValue)
+
+    $CreationDate.Text = "Creation Date:"
+    $CreationDate.AutoSize = $true
+    $CreationDate.Location = New-Object System.Drawing.Point(10, 490)
+    $mainform.Controls.Add($CreationDate)
+
+    $CreationDateValue.Text = ""
+    $CreationDateValue.AutoSize = $true
+    $CreationDateValue.Location = New-Object System.Drawing.Point(90, 490)
+    $mainform.Controls.Add($CreationDateValue)
+
+    $Hash.Text = "Hash:"
+    $Hash.AutoSize = $true
+    $Hash.Location = New-Object System.Drawing.Point(10, 530)
+    $mainform.Controls.Add($Hash)
+
+    $HashValue.Text = ""
+    $HashValue.AutoSize = $true
+    $HashValue.Location = New-Object System.Drawing.Point(55, 530)
+    $mainform.Controls.Add($HashValue)
+
+    $Location.Text = "File Location:"
+    $Location.AutoSize = $true
+    $Location.Location = New-Object System.Drawing.Point(10, 510)
+    $mainform.Controls.Add($Location)
+
+    $LocationValue.Text = ""
+    $LocationValue.AutoSize = $true
+    $LocationValue.Location = New-Object System.Drawing.Point(80, 510)
+    $mainform.Controls.Add($LocationValue)
+
+    $CommandLine.Text = "Command Line Arguments:"
+    $CommandLine.AutoSize = $true
+    $CommandLine.Location = New-Object System.Drawing.Point(10, 550)
+    $mainform.Controls.Add($CommandLine)
+
+    $CommandLineValue.Text = ""
+    $CommandLineValue.AutoSize = $true
+    $CommandLineValue.Location = New-Object System.Drawing.Point(10, 570)
+    $mainform.Controls.Add($CommandLineValue)
+
+
+    # Place Button & Create Button_Clicked event
+    $DetailsLabel.Location = New-Object System.Drawing.Point(10, 400)
+    $mainform.Controls.Add($DetailsLabel)
+    $DetailsLabel.Add_Click(
+    {
+        $P = Get-Ciminstance CIM_Process | ? ProcessID -eq $ProcList.CurrentRow.Cells[1].Value 
+        $PP = Get-Ciminstance CIM_Process | ? ProcessID -eq $P.ParentProcessID 
+        $H = Get-FileHash -Algorithm MD5 $P.ExecutablePath
+        $NameLabelValue.Text = $P.Name
+        $IDLabelValue.Text = $P.ProcessID
+        $ParentIDValue.Text = $P.ParentProcessID
+        $ParentNameValue.Text = $PP.Name
+        $CreationDateValue.Text = $P.CreationDate
+        $LocationValue.Text = $P.ExecutablePath
+        $HashValue.Text = $H.Hash
+        $CommandLineValue.Text = $P.CommandLine
+
+        # write-host $ProcList.SelectedCells.Value
+        write-host $ProcList.CurrentRow.Cells[1].Value
+        })
+
+}
+
+
+# Call Form Creator
+GuiInit
+
+# Call Process Information Creator 
+Proc_Get
+
+# Start Form
+StartForm
